56c8123bebbbc4652b21a50c830b4029ede3bda7
Stanojevic Bojan <Bojanstanojevic89@gmail.com>
2020-08-03 23:37:12 +0200

    new update

--- screens/Checklist.js
+++ screens/Checklist.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text, Platform, StyleSheet } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
+import firebase from '../database/firebaseDb';
 
 export default class Checklist extends React.Component {
-  state = {
-    itemInput: '',
-    centerInput: '',
-  }
+  constructor(props) {
+    super(props);
+
+    this.firestoreRef = firebase.firestore().collection('checklist')
+
+      this.state = {
+        itemInput: '',
+        centerInput: '',
+        myItems: {},
+      }
+
+    }
+
+  componentDidMount() {
+    this.listener = firebase.firestore().collection('checklist').onSnapshot(snap => {
+      const myItems = {}
+      snap.forEach(item => {
+        myItems[item.id] = item.data()
+      })
+      this.setState({myItems})
+    })
+}  
+
+  componentWillUnmount() {
+    this.listener();
+}
 
 
   render () {
@@ -16,28 +39,28 @@ export default class Checklist extends React.Component {
       <View style={styles.container}>
         <View style={{flexDirection: 'row', alignItems: 'stretch', justifyContent: 'flex-start', margin: 15}}>
           <TextInput
-            style={{flex: 6,height: 40,}}
+            style={{flex: 6,height: 50,}}
             label="New item"
             value={itemInput}
             onChangeText={itemInput => this.setState({itemInput})}
           />
-          <Button style={{flex: 2, height: 40, padding: 2, marginLeft: 3,}}  mode="contained" onPress={() => console.log('Pressed')}>
+          <Button style={{flex: 2, height: 50, padding: 2, marginLeft: 3,}}  mode="contained" onPress={() => console.log('Pressed')}>
           Add item
           </Button>
         </View>
 
         <View style={{flexDirection: 'row', alignItems: 'stretch', justifyContent: 'flex-start', margin: 15}}>
           <TextInput
-            style={{flex: 6,height: 40,}}
+            style={{flex: 6,height: 50,}}
             label="New center"
             value={centerInput}
             onChangeText={centerInput => this.setState({centerInput})}
           />
-          <Button style={{flex: 2, height: 40, padding: 2, marginLeft: 3,}}  mode="contained" onPress={() => console.log('Pressed')}>
+          <Button style={{flex: 2, height: 50, padding: 2, marginLeft: 3,}}  mode="contained" onPress={() => console.log('Pressed')}>
           Add center
           </Button>
         </View>
-        {console.log(this.state.centerInput)}
+        {console.log(this.state.myItems)}
       </View>
     )
   }
