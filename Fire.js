34beff0d6aabe8e50098b57c2cdc6b1be4017116
Stanojevic Bojan <bojanstanojevic89@gmail.com>
2020-09-20 12:40:07 +0200

    Update Fire.js

--- Fire.js
+++ Fire.js
@@ -2,14 +2,7 @@ import firebase from 'firebase'
 import '@firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAEaBCJpzH2ukDwzVJ9PbvjNXHai3TkWL8",
-    authDomain: "catering-app-25021.firebaseapp.com",
-    databaseURL: "https://catering-app-25021.firebaseio.com",
-    projectId: "catering-app-25021",
-    storageBucket: "catering-app-25021.appspot.com",
-    messagingSenderId: "704821076290",
-    appId: "1:704821076290:web:699e0f4c2314c33004e8db",
-    measurementId: "G-N570BST9M0"
+    /* FIREBASE CONFIG */
 };
 
 class Fire {
